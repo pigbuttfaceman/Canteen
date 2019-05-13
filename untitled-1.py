c2a8560a61475ed3d498ac9ca20f570dfe2ec7d3
nugg man <37384979+pigbuttfaceman@users.noreply.github.com>
2019-05-14 10:37:36 +1200

    Add files via upload

--- untitled-1.py
+++ untitled-1.py
@@ -2,7 +2,7 @@ from bottle import run, route, view, get, post, request, static_file
 from itertools import count
 
 
-
+#Funtion
 class canteen_content:
 
     _ids = count (0)
@@ -16,34 +16,51 @@ class canteen_content:
         self.food_cost = cost
 
 
-
+#This is a list of times that contain the values for the items
 contents = [
-    canteen_content("Sushi Rolls", "sushirolls.jpg", 5, 0, 5 ),
-    canteen_content("Hot Dog and Chips", "hotdog.jpg", 12, 0, 3 ),
-    canteen_content("Ham Sammie", "hamsammie.jpg", 4, 0, 4)
+    canteen_content("Sushi Rolls", "sushirolls.jpg", 5, 0, 3.50 ),
+    canteen_content("Hot Dog and Chips", "hotdog.jpg", 12, 0, 4 ),
+    canteen_content("Ham Sammie", "hamsammie.jpg", 4, 0, 8)
 ]
 
 
 
 
-#index page
+#index page/the main front page
 @route("/")
 @view ("index")
 def index():
 
     pass
 
-
+#This is the ordering page where the user will choose the food items they wish to have.
 @route("/order") 
 @view("order") 
 def order(): 
     data = dict (contents_list = contents) 
     return data 
 
-
+#This grabs the pictures from the images file
 @route('/picture/<filename>')
 def saved_picture (filename):
     return static_file(filename, root='./images')
+
+#This is the success page the user will see after clicking the order button
+@route('/order_success/<food_id>')
+@view ('order_success')
+def order_success(food_id):
+    
+    food_id = int(food_id)
+    found_food = None
+    for food in contents:
+        if food.id == food_id:
+            found_food = food
+    data = dict (food = found_food)
+    found_food.food_stock = found_food.food_stock - 1
+    found_food.food_sold = found_food.food_sold + 1
+    
+    return data
+
 
 
 
