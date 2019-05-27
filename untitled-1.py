67fdd344a853a05921cde929370e1775dee1a7e9
nugg man <37384979+pigbuttfaceman@users.noreply.github.com>
2019-05-28 10:24:12 +1200

    Add files via upload

--- untitled-1.py
+++ untitled-1.py
@@ -20,7 +20,7 @@ class canteen_content:
 
 #This is a list of times that contain the values for the items
 contents = [
-    canteen_content("Sushi Rolls", "sushirolls.jpg", 5, 0, 3.50, 0, 0 ),
+    canteen_content("Sushi Rolls", "sushirolls.jpg", 64, 0, 3.50, 0, 0 ),
     canteen_content("Hot Dog and Fries", "hotdog.jpg", 12, 0, 4, 0, 0 ),
     canteen_content("Ham Sammie", "hamsammie.jpg", 4, 0, 8, 0, 0)
 ]
@@ -64,7 +64,6 @@ def order_success(food_id):
     found_food.food_stock -= 1  #These plus and minus to the sold and stock variables
     found_food.food_sold += 1
     found_food.food_cart = found_food.food_cart + found_food.food_cost
-    
     return data
 
 
@@ -79,18 +78,18 @@ def plus_stock():
 @route('/add_success/<food_id>', method = 'POST')
 @view ('add_success')
 def add_success(food_id):
-    data = dict (contents_list = contents)
     amount = request.forms.get('amount')
-    
+    amount = int(amount)
     food_id = int(food_id)
     found_food = None
     for food in contents:
         if food.id == food_id:
             found_food = food
     data = dict (food = found_food)
-    found_food.food_stock + amount
+    found_food.food_stock += amount 
     
-    return data  
+    return data
+ 
 
 
 
@@ -98,7 +97,15 @@ def add_success(food_id):
 @route("/user_input/<food_id>")
 @view ("user_input")
 def user_input(food_id):
-    pass
+    data = dict (contents_list = contents)
+    food_id = int(food_id)
+    found_food = None
+    for food in contents:
+        if food.id == food_id:
+            found_food = food
+    data = dict (food = found_food)    
+    return data  
+
 
 
 
